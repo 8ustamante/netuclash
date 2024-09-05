acff937a72b846831ee49dd9063c1acba63db1e9
Bustamante <sebasbga7@gmail.com>
2024-09-05 16:49:39 -0500

    Config update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),],
-  base: 'https://8ustamante.github.io/netuclash/'
-  
-})
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: '/netuclash/' // Establece la base para GitHub Pages
+});
